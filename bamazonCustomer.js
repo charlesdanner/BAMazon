52d86cdeb6f6eeb64ab330b3261fc1a45c523a0c
Charles Danner <danner.ced1@gmail.com>
2019-10-01 11:16:19 -0400

    prompts working so it asks what you'd like to buy and how many.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,36 +22,74 @@ connection.connect(function (err) {
 function afterConnection() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        for(let i = 0; i < res.length; i++){
+        for (let i = 0; i < res.length; i++) {
             console.log(`| ${res[i].id} | ${res[i].product_name} | ${res[i].price} |`)
         }
 
-
+        start()
         //connection.end();
     });
 }
 
 function start() {
     inquirer
-      .prompt({
-        name: "buy",
-        type: "input",
-        message: "What is the ID of the product you would like to purchase?",
-        validate: function(value){
-            if(!isNaN(value)){
-                return true;
-            }else return false;
-        }
-      },)
-      .then(function(answer) {
-        // based on their answer, either call the bid or the post functions
-        if (answer.postOrBid === "POST") {
-          postAuction();
-        }
-        else if(answer.postOrBid === "BID") {
-          bidAuction();
-        } else{
-          connection.end();
-        }
-      });
-  }
+        .prompt([{
+            name: "buy",
+            type: "input",
+            message: "What is the ID of the product you would like to purchase?",
+            validate: function (value) {
+                if (!isNaN(value) && value > 0 && value < 11) {
+                    return true;
+                } else return false;
+            }
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "Okay, how many would you like to purchase?",
+            validate: function (value) {
+                if (!isNaN(value) && value > 0) {
+                    return true;
+                } else return false;
+            }
+        }]
+        )
+        // .then(function (answer) {
+        //     // based on their answer, either call the bid or the post functions
+        //     switch (answer) {
+        //         case 1:
+        //             // code block
+        //             break;
+        //         case 2:
+        //             // code block
+        //             break;
+        //         case 3:
+        //             // code block
+        //             break;
+        //         case 4:
+        //             // code block
+        //             break;
+        //         case 5:
+        //             // code block
+        //             break;
+        //         case 6:
+        //             // code block
+        //             break;
+        //         case 7:
+        //             // code block
+        //             break;
+        //         case 8:
+        //             // code block
+        //             break;
+        //         case 9:
+        //             // code block
+        //             break;
+        //         case 10:
+        //             // code block
+        //             break;
+
+
+        //     }
+        // });
+        connection.end()
+}
