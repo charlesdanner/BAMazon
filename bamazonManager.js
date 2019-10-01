405dd69b8c1b321232f0381771671e1a84532252
Charles Danner <danner.ced1@gmail.com>
2019-10-01 14:40:41 -0400

    switch statement created for user prompt's answer.

---
+++ bamazonManager.js
@@ -0,0 +1,58 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "root1",
+    database: "bamazonDB"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection()
+});
+
+function afterConnection() {
+    connection.query("SELECT * FROM products", function (err, res) {
+
+        start()
+        //connection.end();
+    });
+}
+
+let start = () => {
+    inquirer.prompt([{
+        name: "action",
+        message: "Select Desired Action",
+        type: "list",
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+
+    }]).then(function (inquirerResponse) {
+        switch (inquirerResponse.action) {
+            
+            case "View Products for Sale":
+                // code block
+                break;
+
+            case "View Low Inventory":
+                //do something 
+                break;
+
+            case "Add to Inventory":
+                //do something
+                break;
+
+            case "Add New Product":
+                //do something
+                break;
+
+        }
+    })
+}
