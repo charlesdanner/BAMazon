458926636fb33e24d0d5c7258b2ca88d612c51f1
Charles Danner <danner.ced1@gmail.com>
2019-10-02 18:11:40 -0400

    bamazonSupervisor.js lets users create new departments now.

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,5 +1,6 @@
 let mysql = require("mysql");
 let inquirer = require("inquirer");
+let {table} = require('table');
 
 let connection = mysql.createConnection({
     host: "localhost",
@@ -31,7 +32,38 @@ let afterConnection = () => {
         if(inquirerResponse.supervisorFunctions === "View Product Sales By Department"){
             //do a block of code
         }else if(inquirerResponse.supervisorFunctions === "Create New Department"){
-            
+            inquirer.prompt([
+                {
+                    name: "newDept",
+                    message: "What is the name of the new department you want to create?",
+                    type: "input",
+                    validation: (value) => {
+                        if(value.length > 3 && isNaN(value)){
+                            return true
+                        } return false
+                    }
+                },
+                {
+                    name: "overHead",
+                    message: "What is the estimated overhead this new department will cost?",
+                    type: "input",
+                    validation: (value) => {
+                        if(!isNaN(value) && value > 0){
+                            return true;
+                        } else return false;
+                    }
+                }
+            ]).then(answer => {
+                let newDepartment = answer.newDept;
+                let overHead = answer.overHead;
+                connection.query(`INSERT INTO departments (department_name, over_head_costs) 
+                                        VALUES ('${newDepartment}', ${overHead})`,
+                (err, results) => {
+                    if(err){
+                        console.log(err)
+                    } console.log(`New Department: ${newDepartment} has been successfully created.`);
+                }
+            )})
         }
     })
 }
