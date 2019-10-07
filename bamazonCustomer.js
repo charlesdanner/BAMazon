d6309e12feed3e4c61b4d83b8b1903e5b261ffb4
Charles Danner <danner.ced1@gmail.com>
2019-10-07 13:00:31 -0400

    screen recording made

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -109,3 +109,24 @@ Thank you for your purchase. Your total is $${unitPrice * amountToBuy}
                             `)
                             exit();
 })}})})}
+
+
+
+// INSERT INTO departments (department_name, over_head_costs)
+// 				VALUES	('snacks', 300),
+// 						('toys', 800),
+//                         ('electronics', 2000),
+//                         ('books', 400),
+//                         ('furniture', 1000);
+
+// INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales) 
+// VALUES 	('doritos', 'snacks', 6.50, 50, 650), 
+// 		('gushers', 'snacks', 4.99, 100, 325.25), 
+//         ('barbie', 'toys', 10.99, 40, 307.72),
+//         ('slinky', 'toys', 5.00, 30, 150),
+//         ('xbox', 'electronics', 200, 20, 3000), 
+//         ('playstation4', 'electronics', 250, 20, 3500), 
+//         ('Stranger in a Strange Land', 'books', 10.25, 20, 102.50), 
+//         ('Starship Troopers', 'books', 10.25, 20, 153.75), 
+//         ('chair', 'furniture', 30.99, 20, 1797.42), 
+//         ('sofa', 'furniture', 100.50, 20, 603);
